9b6aac6435eefa44a2aed04d597fa8c1c6a3b049
fantom4545 <166008514+fantom4545@users.noreply.github.com>
2024-05-07 19:46:56 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#import homework12
+#homework12.test()
+#homework12.test1()
+#from homework12 import test , test1
+#test()
+#test1()
+#from homework12 import test as x , test1 as y
+#x()
+#y()
+from homework12 import *
+test()
+test1()
+
+
